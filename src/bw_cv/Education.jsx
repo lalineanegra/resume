c50ac288cba1f74d53c9a01386fbff5b50f48b64
ralonso <rodrigo.gtr@gmail.com>
2021-11-07 18:23:16 -0300

    working in progress

--- src/bw_cv/Education.jsx
+++ src/bw_cv/Education.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import {
-    Grid,
-    Typography
-} from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
-
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 
 const useStyles = makeStyles((theme) => ({
     section: {
@@ -26,7 +30,12 @@ const useStyles = makeStyles((theme) => ({
     },
     title: {
         marginRight: 50
-    }
+    },
+    root: {
+        display: 'flex',
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
 }));
 
 export const Education = (props) => {
@@ -43,7 +52,32 @@ export const Education = (props) => {
                 </Typography>
             </Grid>
             <Grid item xs={12} md={8} className={classes.content}>
-                
+                <List className={classes.root}>
+                    <ListItem>
+                        <ListItemAvatar>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                        <Avatar>
+                            <WorkIcon />
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Work" secondary="Jan 7, 2014" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                        <Avatar>
+                            <BeachAccessIcon />
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Vacation" secondary="July 20, 2014" />
+                    </ListItem>
+                </List>
             </Grid>
         </Grid>
     )
